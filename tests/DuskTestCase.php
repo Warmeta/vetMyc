bfb1b9a062d7e31754b5094ef2d384f94c0ef6ab
JoseAGAA <warmeta@gmail.com>
2017-03-18 05:42:20 +0000

    intentando integracion con codeship

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -18,7 +18,9 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
-        static::startChromeDriver();
+        if (env('DUSK_START_CHROMEDRIVER', true)) {
+            static::startChromeDriver();
+        }
     }
 
     /**
@@ -26,10 +28,20 @@ abstract class DuskTestCase extends BaseTestCase
      *
      * @return \Facebook\WebDriver\Remote\RemoteWebDriver
      */
+
     protected function driver()
     {
+        $chromeOptions = new ChromeOptions();
+        if ($binary = env('DUSK_CHROME_BINARY')) {
+            $chromeOptions->setBinary($binary);
+        }
+        $chromeOptions->addArguments(['no-first-run']);
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $chromeOptions);
+
         return RemoteWebDriver::create(
-            'http://localhost:9515', DesiredCapabilities::chrome()
+            'http://localhost:9515',
+            $capabilities
         );
     }
 }
