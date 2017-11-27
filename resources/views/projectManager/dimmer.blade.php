052c5f91e852c9554ff6bb5a606b20fc7d86cab2
Jose <warmeta@gmail.com>
2017-11-27 16:05:33 +0000

    proj fix

--- resources/views/projectManager/dimmer.blade.php
+++ resources/views/projectManager/dimmer.blade.php
@@ -1,14 +1,16 @@
 <div id="dimmer-{{ $project_id }}" class="panel widget center bgimage" style="margin-bottom:0;overflow:hidden;background-image:url('{{ $image }}');">
-    <div class="dimmer"></div>
-    <div class="panel-content">
-        <a href="/project-manager/{{$project_id}}"><i id="folder-icon" class='icon voyager-folder'></i></a>
-        <div id="RUD">
-            <a href="/project-manager/{{$project_id}}/edit"><i class='icon voyager-pen'></i></a>
-            <a title="Delete" class="delete-project" data-id="{{ $project_id }}" id="delete-{{ $project_id }}" data-route="./project-manager/delete/" data-token="{{ csrf_token() }}">
-                <i class='icon voyager-x'></i>
-            </a>
-        </div>
-        <h4>{{ $project_name }}</h4>
-        <p>{{ $project_type }}</p>
+    <div class="dimmer">
+      <div class="col-xs-3 pull-left"><a href="/project-manager/{{$project_id}}"><i id="folder-icon" class='icon voyager-folder'></i></a></div>
+      <div id="RUD" class="col-xs-3 pull-right">
+        <a href="/project-manager/{{$project_id}}/edit"><i class='icon voyager-pen'></i></a>
+        <a title="Delete" class="delete-project" data-id="{{ $project_id }}" id="delete-{{ $project_id }}" data-route="./project-manager/delete/" data-token="{{ csrf_token() }}">
+          <i class='icon voyager-x'></i>
+        </a>
+      </div>
+    </div>
+    <div class="row panel-content">
+        <div class="col-xs-6"><h4>{{ $project_name }}</h4>
+        <p>{{ $project_type }}</p></div>
+
     </div>
 </div>
---
+++ tests/Feature/Home/HomeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Home;
+
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+  public function testHome()
+  {
+    $response = $this->call('GET', '/', [], [], [], []);
+    $response->assertSee('Bienvenido');
+  }
+
+  public function testMycology()
+  {
+    $response = $this->call('GET', '/mycology/generalidades', [], [], [], []);
+    $response->assertSee('Generalidades');
+  }
+
+  public function testLaboratory()
+  {
+    $response = $this->call('GET', '/lab', [], [], [], []);
+    $response->assertSee('Lista de precios');
+  }
+
+  public function testProjects()
+  {
+    $response = $this->call('GET', '/projects', [], [], [], []);
+    $response->assertSee('Proyectos');
+  }
+}
