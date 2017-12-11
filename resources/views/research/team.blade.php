00094409244532c2f6e706c38c37477bef06838f
warmeta <warmeta@gmail.com>
2017-12-11 04:50:25 +0000

    links to team

--- resources/views/research/team.blade.php
+++ resources/views/research/team.blade.php
@@ -16,7 +16,7 @@
                               <img src="/images/photo/01.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dra. Begoña Acosta Hernández</strong>
+                            <a href="https://orcid.org/0000-0002-0397-4244"><strong>Dra. Begoña Acosta Hernández</strong></a>
                             </br>
                             <p>Micología Clínica en la Universidad Complutense de Madrid y la Universidad de Rovira i Vigili de Reus (Tarragona), PTU de la ULPGC. Unidad de Enfermedades Infecciosas. Facultad de Veterinaria.</p>
                             </br>
@@ -26,7 +26,7 @@
                               <img src="/images/photo/02.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dra. Soraya Déniz Suárez</strong>
+                            <a href="http://www2.ulpgc.es/index.php?pagina=directorio&ver=inicio&v_estado=final_ok&v_id=0000001639"><strong>Dra. Soraya Déniz Suárez</strong></a>
                             </br>
                             <p>PTU de la ULPGC. Unidad de Enfermedades Infecciosas. Facultad de Veterinaria.</p>
                             </br>
@@ -38,7 +38,7 @@
                               <img src="/images/photo/06.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dra. Inmaculada Rosario Medina</strong>
+                            <a href="https://orcid.org/0000-0002-0061-6900"><strong>Dra. Inmaculada Rosario Medina</strong></a>
                             </br>
                             <p>PAL de la ULPGC. Unidad de Enfermedades Infecciosas. Facultad de Veterinaria.</p>
                           </div>
@@ -47,7 +47,7 @@
                               <img src="/images/photo/04.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dra. Otilia Ferrer Quintana</strong>
+                            <a href="http://www2.ulpgc.es/index.php?pagina=directorio&ver=inicio&v_estado=final_ok&v_id=0000001984"><strong>Dra. Otilia Ferrer Quintana</strong></a>
                             </br>
                             <p> PTU de la ULPGC. Colaboradora clínica. Hospital Clínico Vterinario ULPGC.</p>
                           </div>
@@ -58,7 +58,7 @@
                               <img src="/images/photo/05.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dra. Davinia Ortega</strong>
+                            <a href="https://www.cvmesaylopez.es/index.php/veterinarios-mesa-y-lopez-las-palmas"><strong>Dra. Davinia Ortega</strong></a>
                             </br>
                             <p>Dra. en Veterinaria. Colaboradora clínica externa.</p>
                           </div>
@@ -67,7 +67,7 @@
                               <img src="/images/photo/03.jpg" class="photo-img" />
                             </div>
                             </br>
-                            <strong>Dr. Freddy Alejandro Silva Sergent</strong>
+                            <a href="http://lpa.vetcan.org/vet/2023/"><strong>Dr. Freddy Alejandro Silva Sergent</strong></a>
                             </br>
                             <p>Becario postdoctoral del IUSA (instituto Universitario de Sanidad Animal).</p>
                           </div>
