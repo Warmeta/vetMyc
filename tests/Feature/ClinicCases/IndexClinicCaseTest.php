d8fbe52e7057ae8edd6deca10473c17c2c93b992
Jose <warmeta@gmail.com>
2017-12-13 17:51:45 +0000

    added test for filters

--- tests/Feature/ClinicCases/IndexClinicCaseTest.php
+++ tests/Feature/ClinicCases/IndexClinicCaseTest.php
@@ -45,4 +45,172 @@ class IndexClinicCaseTest extends TestCase
     $response->assertStatus(302)
       ->assertRedirect('/');
   }
+
+  public function testIndexFilterStateInProgressClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['clinic_case_status' => 'inprogress'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=inprogress');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterStateInProgressClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['clinic_case_status' => 'finished'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=inprogress');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterStateFinishedClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['clinic_case_status' => 'finished'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=finished');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterStateFinishedClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['clinic_case_status' => 'inprogress'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=finished');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterBacterialIsolateClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['bacterial_isolate' => 'test'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=bacterial_isolate');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterBacterialIsolateClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['bacterial_isolate' => null])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=bacterial_isolate');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterFungiIsolateClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['fungi_isolate' => 'test'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=fungi_isolate');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterFungiIsolateClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['fungi_isolate' => null])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=&filter=fungi_isolate');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterLocalizationClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['localization' => 'ear'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=ear&number_clinic_history=&filter=localization');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterLocalizationClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['localization' => 'skin'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=ear&number_clinic_history=&filter=localization');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterNumberClinicCaseAsAdminSuccess()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['number_clinic_history' => '321'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=321&filter=number_clinic_history');
+    $response->assertStatus(200)
+    ->assertSee((string)$clinicCase['number_clinic_history']);
+  }
+
+  public function testIndexFilterNumberClinicCaseAsAdminFailWithNoClinicCaseAssociated()
+  {
+    $clinicCase = factory('App\ClinicCase')->create(['number_clinic_history' => '123'])->toArray();
+    $this->assertDatabaseHas('clinic_cases', $clinicCase);
+
+    $user = $this->createUserWithAdminPermissions('clinic_cases');
+
+    $response = $this
+    ->actingAs($user)
+    ->get('/lab/clinic-case?localization=&number_clinic_history=321&filter=number_clinic_history');
+    $response->assertStatus(200)
+    ->assertDontSee((string)$clinicCase['number_clinic_history']);
+  }
 }
